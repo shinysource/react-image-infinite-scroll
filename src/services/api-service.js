5713ce77c03acdb7af3a7b17a3e768d2163325fd
shinysource <highly.flores@gmail.com>
2022-07-23 21:36:11 -0700

    Added api service

---
+++ src/services/api-service.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const unspalshApiKey = process.env.REACT_APP_UNSPLASH_API_KEY;
+const baseURL = process.env.REACT_APP_UNSPLASH_BASE_URL;
+
+const createAxiosInstance = (baseURL) => {
+  const instance = axios.create({
+    baseURL,
+  });
+  instance.interceptors.request.use((config) => {
+    config.headers = {
+      Authorization: `CLIENT-ID ${unspalshApiKey}`,
+    };
+    return config;
+  });
+
+  return instance;
+};
+
+const gateway = createAxiosInstance(baseURL, 30000);
+
+const apiService = (pageNumber) => gateway.get("/photos/?page=" + pageNumber);
+
+export default apiService;
